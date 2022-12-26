aa1b49490e5b6f33cd62eed8282f105b3ba9f6e3
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-12-26 16:34:09 +0530

    Initial Commit

---
+++ PrivateFileSharingApp/PrivateFileSharingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateFileSharingAppApp.swift
+//  PrivateFileSharingApp
+//
+//  Created by Bhumika Patel on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PrivateFileSharingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
